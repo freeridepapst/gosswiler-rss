eacb17af743bcf4cc03b5c368a64589bc4294caf
freeridepapst <freeridepapst@gmail.com>
2026-02-02 08:48:40 +0100

    Update rss.js

--- api/rss.js
+++ api/rss.js
@@ -1,16 +1,40 @@
+import fetch from "node-fetch";
 import cheerio from "cheerio";
 
 export default async function handler(req, res) {
-  const url = "https://www.gosswiler.com/blog/";
+  try {
+    const sitemapUrl = "https://www.gosswiler.com/sitemap.xml";
 
-  const r = await fetch(url, {
-    headers: {
-      "User-Agent": "Mozilla/5.0"
-    }
-  });
+    const r = await fetch(sitemapUrl, {
+      headers: { "User-Agent": "Mozilla/5.0" }
+    });
 
-  const html = await r.text();
+    const xml = await r.text();
 
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(html.substring(0, 3000));
+    const $ = cheerio.load(xml, {
+      xmlMode: true
+    });
+
+    const entries = [];
+
+    $("url").each((_, el) => {
+      const loc = $(el).find("loc").text();
+      const lastmod = $(el).find("lastmod").text();
+
+      if (loc.includes("/blog/")) {
+        entries.push({ loc, lastmod });
+      }
+    });
+
+    res.status(200).json({
+      totalUrls: $("url").length,
+      blogPosts: entries.length,
+      sample: entries.slice(0, 5)
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
 }
