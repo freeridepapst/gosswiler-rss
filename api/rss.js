fed852a344d28d3e6e09956f89e4a9d01a00cfc8
freeridepapst <freeridepapst@gmail.com>
2026-02-06 12:55:13 +0100

    Update rss.js

--- api/rss.js
+++ api/rss.js
@@ -1,55 +1,31 @@
 export default async function handler(req, res) {
   try {
-    const sitemapUrl = "https://www.gosswiler.com/sitemap.xml?nocache=1";
+    const url = "https://www.gosswiler.com/sitemap.xml";
 
-    const response = await fetch(sitemapUrl, {
+    const r = await fetch(url, {
       headers: {
         "User-Agent": "Mozilla/5.0",
-        Accept: "application/xml"
+        Accept: "*/*"
       }
     });
 
-    const xml = await response.text();
+    const text = await r.text();
 
-    // extract all loc entries (ignore lastmod completely)
-    const urls = [...xml.matchAll(/<loc>(.*?)<\/loc>/g)]
-      .map(m => m[1].trim())
-      .filter(u => u.includes("/blog/") && u !== "https://www.gosswiler.com/blog/");
+    const locs = [...text.matchAll(/<loc>(.*?)<\/loc>/g)].map(m => m[1]);
 
-    if (!urls.length) throw new Error("No blog posts");
-
-    // newest is LAST in imagevuex sitemap
-    const newest = urls[urls.length - 1];
-
-    const title = newest
-      .split("/")
-      .filter(Boolean)
-      .pop()
-      .replace(/-/g, " ");
-
-    const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
-<channel>
-<title>Gosswiler Blog</title>
-<link>https://www.gosswiler.com/blog/</link>
-<description>Latest blog post</description>
-
-<item>
-<title><![CDATA[${title}]]></title>
-<link>${newest}</link>
-<guid>${newest}</guid>
-<description><![CDATA[New blog post on gosswiler.com]]></description>
-</item>
-
-</channel>
-</rss>`;
-
-    res.setHeader("Content-Type", "application/rss+xml");
-    res.setHeader("Cache-Control", "no-store");
-
-    res.status(200).send(rss);
+    res.status(200).json({
+      ok: true,
+      httpStatus: r.status,
+      textSample: text.slice(0, 500),
+      locCount: locs.length,
+      first10: locs.slice(0, 10),
+      last10: locs.slice(-10)
+    });
 
   } catch (e) {
-    res.status(500).json({ ok: false, error: e.message });
+    res.status(200).json({
+      ok: false,
+      error: e.message
+    });
   }
 }
